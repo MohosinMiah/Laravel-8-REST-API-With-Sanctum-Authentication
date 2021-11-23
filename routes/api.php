7115eda5fd2113d733d1ee9a11c88e191e9fbd9c
mohosin16103308 <hamza1610330816@gmail.com>
2021-11-23 19:33:20 +0600

    Added Product Model and Table

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+Route::get('/products', function() {
+    $text = "Here will display all products";
+    return $text;
+});
+
+
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
